f727fe11a4a00acb2314e90cf64a0ebee9216eda
Sebastian Celestino <scelestino@gmail.com>
2021-06-22 00:24:14 +0200

    add total balance

--- packages/hardhat/contracts/Pool.sol
+++ packages/hardhat/contracts/Pool.sol
@@ -1,16 +1,21 @@
 pragma solidity >=0.6.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
-import '@openzeppelin/contracts/math/SafeMath.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+import '@uniswap/v3-core/contracts/libraries/LowGasSafeMath.sol';
 
 contract Pool {
-  using SafeMath for uint256;
   using SafeERC20 for IERC20;
+  using LowGasSafeMath for uint256;
 
   IERC20 token;
-  mapping (address => uint) wallets;
+
+  uint256 totalBalance = 0;
+  mapping (address => uint) balances;
+
+  uint256 totalShare = 0;
+  mapping (address => uint) shares;
 
   constructor (address _token) {
     token = IERC20(_token);
@@ -18,18 +23,25 @@ contract Pool {
 
   function deposit(uint amount) external {
     SafeERC20.safeTransferFrom(token, msg.sender, address(this), amount);
-    wallets[msg.sender] = wallets[msg.sender].add(amount);
+    balances[msg.sender] = balances[msg.sender].add(amount);
+    totalBalance = totalBalance.add(amount);
+  }
+
+  function depositFee(uint amount) external {
+    SafeERC20.safeTransferFrom(token, msg.sender, address(this), amount);
+    totalBalance = totalBalance.add(amount);
   }
 
   function withdraw(uint amount) external {
-    uint balance = wallets[msg.sender];
+    uint balance = balances[msg.sender];
     require(balance >= amount, "Pool: not enough balance");
     token.safeTransfer(msg.sender, amount);
-    wallets[msg.sender] = balance.sub(amount);
+    balances[msg.sender] = balance.sub(amount);
+    totalBalance = totalBalance.sub(amount);
   }
 
   function wallet(address owner) external view returns (uint) {
-    return wallets[owner];
+    return balances[owner];
   }
 
 }

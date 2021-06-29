9c9497b51b8b8bf7aceb500d6b394da7883cd921
Sebastian Celestino <scelestino@gmail.com>
2021-06-29 02:35:02 +0200

    add weth and dai implementations of Pool

---
+++ packages/hardhat/contracts/WETHPool.sol
@@ -0,0 +1,7 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+import "./Pool.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract WETHPool is Pool(ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)) {
+}

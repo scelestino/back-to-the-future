3e78f76d1bb7755e94a0151d921082ea2842aa0b
Sebastián Celestino <768503+scelestino@users.noreply.github.com>
2021-06-29 12:18:25 +0200

    Fix deploy (#13)
    * add weth and dai implementations of Pool
    
    * make deploy.js deploy our contracts
    
    * result of deployment
    
    * make fork use alchemy + same block we are using for E2E

---
+++ packages/hardhat/contracts/DAIPool.sol
@@ -0,0 +1,7 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+import "./Pool.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract DAIPool is Pool(ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F)) {
+}
--- packages/hardhat/scripts/deploy.js
+++ packages/hardhat/scripts/deploy.js
@@ -5,11 +5,16 @@ const { config, ethers, tenderly, run } = require("hardhat");
 const { utils } = require("ethers");
 const R = require("ramda");
 
+const uniswapFactory = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+
 const main = async () => {
 
   console.log("\n\n 📡 Deploying...\n");
 
-  const yourContract = await deploy("YourContract") // <-- add in constructor args like line 19 vvvv
+  const wethPoolContract = await deploy("WETHPool")
+  const usdtPoolContract = await deploy("DAIPool")
+  const userAccountContract = await deploy("UserAccount")
+  const futureContract = await deploy("Future", [wethPoolContract.address, usdtPoolContract.address, 5, uniswapFactory])
 
   //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
   //const secondContract = await deploy("SecondContract")

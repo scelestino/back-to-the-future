37f9424a4d4a05def534f5ab30bedd056dcecd22
Bruno <bruno.bonanno@weareadaptive.com>
2021-06-30 13:33:42 +0200

    add params to deployed pools

--- packages/hardhat/contracts/DAIPool.sol
+++ packages/hardhat/contracts/DAIPool.sol
@@ -3,5 +3,5 @@ pragma solidity >=0.6.0 <0.9.0;
 import "./Pool.sol";
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract DAIPool is Pool(ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F)) {
+contract DAIPool is Pool(ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F), 0, 0, 0, 0) {
 }
--- packages/hardhat/contracts/WETHPool.sol
+++ packages/hardhat/contracts/WETHPool.sol
@@ -3,5 +3,5 @@ pragma solidity >=0.6.0 <0.9.0;
 import "./Pool.sol";
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract WETHPool is Pool(ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)) {
+contract WETHPool is Pool(ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2), 0, 0, 0, 0) {
 }

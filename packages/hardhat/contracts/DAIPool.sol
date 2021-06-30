3955632de44abf57ee987a698ec9bee10a6cf400
Bruno <bruno.bonanno@weareadaptive.com>
2021-06-30 15:37:18 +0200

    add borrow params to deploy pools

--- packages/hardhat/contracts/DAIPool.sol
+++ packages/hardhat/contracts/DAIPool.sol
@@ -4,5 +4,5 @@ pragma solidity >=0.6.0 <0.9.0;
 import "./Pool.sol";
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract DAIPool is Pool(ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F), 0, 0, 0, 0) {
+contract DAIPool is Pool(ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F), 800000000000000000, 0, 40000000000000000, 750000000000000000) {
 }
--- packages/hardhat/contracts/WETHPool.sol
+++ packages/hardhat/contracts/WETHPool.sol
@@ -4,5 +4,5 @@ pragma solidity >=0.6.0 <0.9.0;
 import "./Pool.sol";
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract WETHPool is Pool(ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2), 0, 0, 0, 0) {
+contract WETHPool is Pool(ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2), 650000000000000000, 0, 80000000000000000, 1000000000000000000) {
 }

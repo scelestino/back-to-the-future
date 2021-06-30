e30c06c3d2602d09fbac8f9e4c694e109e233d84
Sebastián Celestino <768503+scelestino@users.noreply.github.com>
2021-07-01 01:19:01 +0200

    change publish script to publish DAI and WETH contracts (so we can use them from UI) (#18)

--- packages/hardhat/scripts/publish.js
+++ packages/hardhat/scripts/publish.js
@@ -5,18 +5,21 @@ const bre = require("hardhat");
 const publishDir = "../react-app/src/contracts";
 const graphDir = "../subgraph"
 
-function publishContract(contractName) {
+function publishContract(prefix, contractName, alias = "", fixedAddress = "") {
+
+  const name = alias != "" ? alias : contractName
+
   console.log(
     " 💽 Publishing",
-    chalk.cyan(contractName),
+    chalk.cyan(name),
     "to",
     chalk.gray(publishDir)
   );
   try {
     let contract = fs
-      .readFileSync(`${bre.config.paths.artifacts}/contracts/${contractName}.sol/${contractName}.json`)
+      .readFileSync(`${bre.config.paths.artifacts}/contracts/${prefix}${contractName}.sol/${contractName}.json`)
       .toString();
-    const address = fs
+    const address = fixedAddress != "" ? fixedAddress : fs
       .readFileSync(`${bre.config.paths.artifacts}/${contractName}.address`)
       .toString();
     contract = JSON.parse(contract);
@@ -35,17 +38,17 @@ function publishContract(contractName) {
       }
 
     graphConfig = JSON.parse(graphConfig)
-    graphConfig[contractName + "Address"] = address
+    graphConfig[name + "Address"] = address
     fs.writeFileSync(
-      `${publishDir}/${contractName}.address.js`,
+      `${publishDir}/${name}.address.js`,
       `module.exports = "${address}";`
     );
     fs.writeFileSync(
-      `${publishDir}/${contractName}.abi.js`,
+      `${publishDir}/${name}.abi.js`,
       `module.exports = ${JSON.stringify(contract.abi, null, 2)};`
     );
     fs.writeFileSync(
-      `${publishDir}/${contractName}.bytecode.js`,
+      `${publishDir}/${name}.bytecode.js`,
       `module.exports = "${contract.bytecode}";`
     );
 
@@ -58,16 +61,16 @@ function publishContract(contractName) {
       JSON.stringify(graphConfig, null, 2)
     );
     fs.writeFileSync(
-      `${graphDir}/abis/${contractName}.json`,
+      `${graphDir}/abis/${name}.json`,
       JSON.stringify(contract.abi, null, 2)
     );
 
-    console.log(" 📠 Published "+chalk.green(contractName)+" to the frontend.")
+    console.log(" 📠 Published "+chalk.green(name)+" to the frontend.")
 
     return true;
   } catch (e) {
     if(e.toString().indexOf("no such file or directory")>=0){
-      console.log(chalk.yellow(" ⚠️  Can't publish "+contractName+" yet (make sure it getting deployed)."))
+      console.log(chalk.yellow(" ⚠️  Can't publish "+name+" yet (make sure it getting deployed)."))
     }else{
       console.log(e);
       return false;
@@ -84,15 +87,23 @@ async function main() {
     if (file.indexOf(".sol") >= 0) {
       const contractName = file.replace(".sol", "");
       // Add contract to list if publishing is successful
-      if (publishContract(contractName)) {
+      if (publishContract("", contractName)) {
         finalContractList.push(contractName);
       }
     }
   });
+
+  publishContract("stubs/", "ERC20Stub", "DAI", "0x6b175474e89094c44da98b954eedeac495271d0f")
+  finalContractList.push("DAI")
+
+  publishContract("stubs/", "IWETH9", "WETH", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
+  finalContractList.push("WETH")
+
   fs.writeFileSync(
     `${publishDir}/contracts.js`,
     `module.exports = ${JSON.stringify(finalContractList)};`
   );
+
 }
 main()
   .then(() => process.exit(0))
